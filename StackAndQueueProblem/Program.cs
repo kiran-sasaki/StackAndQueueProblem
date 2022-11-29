107941decd8534e7e2d183859f2bd4b0a5619974
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-29 16:17:13 +0530

    Added Welcome Message For StackAndQueue Problem

---
+++ StackAndQueueProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackAndQueueProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome the SatckAndQueue Problem");
+        }
+    }
+}
