f7dd27bbaad2f73a36663ab1d23c33f662d9c13c
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-30 12:03:36 +0530

    Added create Stack function For StackAndQueue Problem

--- StackAndQueueProblem/Program.cs
+++ StackAndQueueProblem/Program.cs
@@ -4,7 +4,11 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome the SatckAndQueue Problem");
+            Stack stack = new Stack();
+            stack.Insert(70);
+            stack.Insert(30);
+            stack.Insert(56);
+            stack.Display();
         }
     }
 }
