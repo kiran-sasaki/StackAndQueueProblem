b0a7878a4bb4dfc39770a8870cf89487078c4862
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-30 12:42:16 +0530

    Added create Queue function For StackAndQueue Problem

--- StackAndQueueProblem/Program.cs
+++ StackAndQueueProblem/Program.cs
@@ -4,14 +4,11 @@
     {
         public static void Main(string[] args)
         {
-            Stack stack = new Stack();
-            stack.Insert(70);
-            stack.Insert(30);
-            stack.Insert(56);
-            stack.Display();
-            stack.Peek();
-            stack.Pop();
-            stack.IsEmpty();
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(70);
+            queue.Enqueue(30);
+            queue.Display();
         }
     }
 }
---
+++ StackAndQueueProblem/Queue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueProblem
+{
+    public class Queue
+    {
+        Node head = null;
+        //Enqueue Method For Inserting A value in Queue
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted to queue", node.data);
+
+        }
+        //Display method display the value
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
